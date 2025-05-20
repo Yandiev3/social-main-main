3b8b69637cc6c4b1c31f1c087a683ef4c70d6469
yandiev3 <yandiev3@mail.ru>
2025-05-20 10:33:06 +0300

    profile-page

--- tik-talk/src/app/app.routes.ts
+++ tik-talk/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
-    {path: '', component: ProfileCardComponent}
-
+    {path: '', component: ProfileCardComponent},
+    {path: 'profile', component: ProfilePageComponent}
 ];
---
+++ tik-talk/src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-profile-page',
+    standalone: true,
+    imports: [],
+    templateUrl: './profile-page.component.html',
+    styleUrls: ['./profile-page.component.scss']
+  })
+
+  export class ProfilePageComponent{
+        
+  }
